a272bec0ddbeb7a99e0f2439da120c89db8ade8c
Paul Fadayo <paulfadayo@yahoo.com>
2023-08-22 19:21:15 +0100

    split component UI

--- src/App.js
+++ src/App.js
@@ -142,24 +142,32 @@ function ListBox() {
   );
 }
 
+// movie--list
 function MovieList() {
   const [movies, setMovies] = useState(tempMovieData);
 
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <Movie movie={movie} key={movie.imdbID} />
       ))}
     </ul>
+  );
+}
+
+// movie
+function Movie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
