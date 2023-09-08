b197e2af8194fc88ada0e56b927b4f9465173a77
Paul Fadayo <paulfadayo@yahoo.com>
2023-09-08 16:40:54 +0100

    added a star rating component

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -59,7 +60,17 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </Navbar>
-      <Main movies={movies} />
+
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+      </Main>
     </>
   );
 }
@@ -119,32 +130,49 @@ Main Component
 ==================================================================
 */
 
-const Main = ({ movies }) => {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+const Main = ({ children }) => {
+  return <main className="main">{children}</main>;
 };
 
-// list--box
-function ListBox({ movies }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+// Box
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "-" : "+"}
+      </button>
+
+      {isOpen && children}
+    </div>
+  );
+}
+
+/*
+// watched--box
+function WatchedBox() {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
 
   return (
     <div className="box">
       <button
         className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
+        onClick={() => setIsOpen2((open) => !open)}
       >
-        {isOpen1 ? "-" : "+"}
+        {isOpen2 ? "-" : "+"}
       </button>
-
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen2 && (
+        <>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </>
+      )}
     </div>
-  );
+    );
 }
+*/
 
 // movie--list
 function MovieList({ movies }) {
@@ -170,29 +198,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-// watched--box
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "-" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-        </>
-      )}
-    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        color="black"
+        size={25}
+        defaultRating={0}
+        onSetRating={setMovieRating}
+      />
+      <p>
+        This Movie was rated <b>{movieRating}</b> stars!
+      </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <StarRating
+      maxRating={6}
+      defaultRating={4}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing", "Excellent"]}
+    />
+    <StarRating color="green" size={24} className="test" defaultRating={3} /> */}
+
+    <Test />
   </React.StrictMode>
 );
-
-
