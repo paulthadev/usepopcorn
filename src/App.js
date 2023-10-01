20b0bacd50c3bdec50ad8bf6deb29fb6836a7930
Paul Fadayo <paulfadayo@yahoo.com>
2023-10-01 17:17:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ export default function App() {
     async function fetchMovies() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
