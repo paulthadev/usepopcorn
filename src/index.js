089f15197501da78465038e35afea0383da29504
Paul Fadayo <paulfadayo@yahoo.com>
2023-10-01 01:22:28 +0100

    UI clean up

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
-
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating
-        maxRating={5}
-        color="black"
-        size={25}
-        defaultRating={0}
-        onSetRating={setMovieRating}
-      />
-      <p>
-        This Movie was rated <b>{movieRating}</b> stars!
-      </p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating
-      maxRating={6}
-      defaultRating={4}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing", "Excellent"]}
-    />
-    <StarRating color="green" size={24} className="test" defaultRating={3} /> */}
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
